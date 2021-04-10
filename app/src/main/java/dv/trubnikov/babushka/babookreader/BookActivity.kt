66485502802488e3d476db2f98363e3983588a8e
Dima Trubnikov <dvtrubnikov@sberbank.ru>
2021-04-10 19:48:21 +0300

    Initial commit

---
+++ app/src/main/java/dv/trubnikov/babushka/babookreader/BookActivity.kt
@@ -0,0 +1,11 @@
+package dv.trubnikov.babushka.babookreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/dv/trubnikov/babushka/babookreader/BookViewModel.kt
@@ -0,0 +1,12 @@
+package dv.trubnikov.babushka.babookreader
+
+import androidx.lifecycle.ViewModel
+
+/**
+ *
+ *
+ * @author Дмитрий Трубников on 10.04.2021
+ */
+class BookViewModel : ViewModel() {
+
+}
